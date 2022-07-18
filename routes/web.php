1323df6ae2eb3ca0a41c32e68cfcc35b2ce5642b
Sergio Morgan <morgan.sergio@gmail.com>
2022-07-17 19:06:08 -0500

    test

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,14 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
 });
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('dashboard',                \App\Http\Livewire\ShowDashboard::class)                ->name('dashboard.index');
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('users',                    \App\Http\Livewire\users\ShowUsers::class)              ->name('users.index');
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('users/create',             \App\Http\Livewire\users\CreateUser::class)             ->name('users.create');
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('users/{item}/edit',        \App\Http\Livewire\users\CreateUser::class)             ->name('users.edit');
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('sites',                    \App\Http\Livewire\sites\ShowSites::class)              ->name('sites.index');
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('sites/create',             \App\Http\Livewire\sites\CreateSites::class)            ->name('sites.create');
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('sites/{item}/edit',        \App\Http\Livewire\sites\CreateSites::class)            ->name('sites.edit');
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('ostickets',                \App\Http\Livewire\ostickets\ShowOstickets::class)      ->name('ostickets.index');
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('ostickets/create',         \App\Http\Livewire\ostickets\CreateOstickets::class)    ->name('ostickets.create');
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('ostickets/{item}/edit',    \App\Http\Livewire\ostickets\EditOstickets::class)      ->name('ostickets.edit');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('users',                    \App\Http\Livewire\Users\ShowUsers::class)              ->name('users.index');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('users/create',             \App\Http\Livewire\Users\CreateUser::class)             ->name('users.create');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('users/{item}/edit',        \App\Http\Livewire\Users\CreateUser::class)             ->name('users.edit');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('sites',                    \App\Http\Livewire\Sites\ShowSites::class)              ->name('sites.index');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('sites/create',             \App\Http\Livewire\Sites\CreateSites::class)            ->name('sites.create');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('sites/{item}/edit',        \App\Http\Livewire\Sites\CreateSites::class)            ->name('sites.edit');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('ostickets',                \App\Http\Livewire\Ostickets\ShowOstickets::class)      ->name('ostickets.index');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('ostickets/create',         \App\Http\Livewire\Ostickets\EditOstickets::class)      ->name('ostickets.edit');
 
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified' ])->get('ostickets/excel/export',  '\App\Http\Livewire\ostickets\ShowOstickets@export')      ->name('ostickets.export');
